27ea30930d073f9780b9e26492c034b5e025907c
Jacob Toelke <jrtoel07@thomasmore.edu>
2023-04-01 11:59:39 -0400

    Test Model

--- NewsAssignment/Models/Test.cs
+++ NewsAssignment/Models/Test.cs
@@ -2,5 +2,19 @@
 {
     public class Test
     {
+        public string? title { get; set; }
+        public string? link { get; set; } 
+        public List<string>? keywords { get; set; }
+        public List<string>? creators { get; set; }
+        public string? videoURL { get; set; }
+        public string? description { get; set; }
+        public string? content { get; set; }
+        public string? pubDate { get; set; }
+        public string? fullDescription { get; set;}
+        public string? imageURL { get; set;}
+        public string? sourceID { get; set;}
+        public List<string>? countries { get; set;}
+        public List<string>? categories { get; set;}
+        public string? language { get; set; }
     }
 }
