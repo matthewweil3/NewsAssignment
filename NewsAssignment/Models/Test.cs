8a249ff7b03aecbb8ecd3deca1041ee139474ec4
jacob <jacob@LAPTOP-9UFQ59OG>
2023-04-04 15:50:03 -0400

    Changes to Test class and page.

--- NewsAssignment/Models/Test.cs
+++ NewsAssignment/Models/Test.cs
@@ -4,18 +4,18 @@
     {
         public int Id { get; set; }
         public string? title { get; set; }
-        public string? link { get; set; } 
+        public string? link { get; set; }
         public List<string>? keywords { get; set; }
         public List<string>? creators { get; set; }
         public string? videoURL { get; set; }
         public string? description { get; set; }
         public string? content { get; set; }
         public string? pubDate { get; set; }
-        public string? fullDescription { get; set;}
-        public string? imageURL { get; set;}
-        public string? sourceID { get; set;}
-        public List<string>? countries { get; set;}
-        public List<string>? categories { get; set;}
+        public string? fullDescription { get; set; }
+        public string? imageURL { get; set; }
+        public string? sourceID { get; set; }
+        public List<string>? countries { get; set; }
+        public List<string>? categories { get; set; }
         public string? language { get; set; }
     }
 }
