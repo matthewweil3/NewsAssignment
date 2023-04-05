413d128bcd58102a44809cab0e52fb3dfd0194e6
Jacob Toelke <jrtoel07@thomasmore.edu>
2023-04-05 13:18:31 -0400

    Changes to TestList and TestPage.

--- NewsAssignment/Models/TestList.cs
+++ NewsAssignment/Models/TestList.cs
@@ -2,5 +2,7 @@
 {
     public class TestList
     {
+        public int Id { get; set; }
+        public List<Test>? test { get; set; }
     }
 }
--- NewsAssignment/Pages/TestPage.cshtml.cs
+++ NewsAssignment/Pages/TestPage.cshtml.cs
@@ -5,8 +5,40 @@ namespace NewsAssignment.Pages
 {
     public class TestPageModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        //private readonly ILogger<IndexModel> _logger;
+
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IList<Test> tests { get; set; }
+        //public void OnGet()
+        //{
+        //    tests = new List<Test>();
+        //}
+
+        //[BindProperty]
+        //public string topic { get; set; }
+        //public async Task<IActionResult> OnPost()
+        //{
+        //    Uri mb = new Uri(" https://musicbrainz.org/ws/2/artist/?query=" + topic + "&fmt=json");
+
+        //    HttpClient client = new HttpClient();
+        //    //not necessary
+        //    client.DefaultRequestHeaders.Add("User-Agent", "tmu.edu");
+
+        //    HttpResponseMessage response = await client.GetAsync(mb.ToString());
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string data = await response.Content.ReadAsStringAsync();
+        //        var TestResults = JsonConvert.DeserializeObject<List<Test>>(data);
+        //        tests = TestResults;
+        //    }
+        //    //TestList retval = new TestList();
+        //    //retval.tests = tests;
+        //    //return Page();
+        //}
     }
 }
