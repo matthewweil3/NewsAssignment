d32b13e0168735b9fadac511c3b61a98af2cf4f5
Mackyle Borchers <mjborc14@thomasmore.edu>
2023-04-17 09:53:02 -0400

    Ad Page Building

---
+++ NewsAssignment/Pages/Ads/Ad.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewsAssignment.Pages.Ads
+{
+    public class AdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
