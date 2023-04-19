73fcefef97e0f2fb407477b87aeec298178db71c
Foxbeast <impanismicah@gmail.com>
2023-04-19 10:46:43 -0400

    Added comment CRUD

---
+++ NewsAssignment/Pages/Comments/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NewsAssignment.Data;
+using NewsAssignment.Models;
+
+namespace NewsAssignment.Pages.Comments
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly NewsAssignment.Data.NewsAssignmentCommentContext _context;
+
+        public DetailsModel(NewsAssignment.Data.NewsAssignmentCommentContext context)
+        {
+            _context = context;
+        }
+
+      public Comment Comment { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Comment == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comment.FirstOrDefaultAsync(m => m.Id == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Comment = comment;
+            }
+            return Page();
+        }
+    }
+}
---
+++ NewsAssignment/Pages/Comments/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NewsAssignment.Data;
+using NewsAssignment.Models;
+
+namespace NewsAssignment.Pages.Comments
+{
+    public class EditModel : PageModel
+    {
+        private readonly NewsAssignment.Data.NewsAssignmentCommentContext _context;
+
+        public EditModel(NewsAssignment.Data.NewsAssignmentCommentContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Comment Comment { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Comment == null)
+            {
+                return NotFound();
+            }
+
+            var comment =  await _context.Comment.FirstOrDefaultAsync(m => m.Id == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            Comment = comment;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Comment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(Comment.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CommentExists(int id)
+        {
+          return (_context.Comment?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
