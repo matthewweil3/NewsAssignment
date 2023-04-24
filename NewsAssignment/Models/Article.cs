3147ad643dae65a0100eea16c59927f7fd0f6a80
Charles Zimmer <zimmerc@thomasmore.edu>
2023-04-24 11:00:02 -0400

    For Jacob

--- NewsAssignment/Models/Article.cs
+++ NewsAssignment/Models/Article.cs
@@ -10,18 +10,18 @@ namespace NewsAssignment.Models
         public string? link { get; set; }
         public string? source_id { get; set; }
         //public string? keywords { get; set; }
-        public List<string>? keywords { get; set; }
+        //public List<string>? keywords { get; set; }
         //public string? creator { get; set; }
-        public List<string>? creator { get; set; }
+        //public List<string>? creator { get; set; }
         public string? image_url { get; set; }
         public string? video_url { get; set; }
         public string? description { get; set; }
         public string? pubDate { get; set; }
         public string? content { get; set; }
         //public string? country { get; set; }
-        public List<string>? country { get; set; }
+        //public List<string>? country { get; set; }
         //public List<Category>? category { get; set; }
-        public List<string>? category { get; set; }
+        //public List<string>? category { get; set; }
         public string? language { get; set; }
         public string? fullDescription { get; set; }
     }
