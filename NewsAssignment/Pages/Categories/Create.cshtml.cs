0051af090b7cf6ec7d5e3ae8b08418ab29df62d1
Matthew Weil <mgweil29@thomasmore.edu>
2023-04-25 22:22:14 -0400

    Users able to select favorite topics

--- NewsAssignment/Pages/Categories/Create.cshtml.cs
+++ NewsAssignment/Pages/Categories/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,6 +12,7 @@ using NewsAssignment.Models;
 
 namespace NewsAssignment.Pages.Categories
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly NewsAssignment.Data.ApplicationDbContext _context;
@@ -25,7 +27,6 @@ namespace NewsAssignment.Pages.Categories
 
         public IActionResult OnGet()
         {
-            storeID = _userManager.GetUserId(HttpContext.User);
             ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "display");
             return Page();
         }
@@ -37,13 +38,15 @@ namespace NewsAssignment.Pages.Categories
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.ApplicationUserCategory == null || ApplicationUserCategory == null)
+            storeID = _userManager.GetUserId(HttpContext.User);
+            var cats = Request.Form["selected"];
+            for(int i = 0; i < 5; i++)  //loops 5 times because only select 5 categories
             {
-                return Page();
+                var APUC = new ApplicationUserCategory { ApplicationUserId = storeID, CategoryId = Int32.Parse(cats[i]) };
+                _context.ApplicationUserCategory.Add(APUC);
+                await _context.SaveChangesAsync();
             }
-
-            _context.ApplicationUserCategory.Add(ApplicationUserCategory);
-            await _context.SaveChangesAsync();
+            
 
             return RedirectToPage("./Index");
         }
