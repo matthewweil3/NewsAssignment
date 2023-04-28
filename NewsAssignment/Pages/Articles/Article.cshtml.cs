bac6fcca587df7d9f78d1091356a731442e1ba0f
Foxbeast <impanismicah@gmail.com>
2023-04-28 10:51:01 -0400

    Viewdata Partial Page Comment Create Side

--- NewsAssignment/Pages/Articles/Article.cshtml.cs
+++ NewsAssignment/Pages/Articles/Article.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NewsAssignment.Models;
 
+
 namespace NewsAssignment.Pages.Articles
 {
     public class ArticleModel : PageModel
