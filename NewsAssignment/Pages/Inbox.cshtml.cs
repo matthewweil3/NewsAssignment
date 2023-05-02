d2d4e71091fe87dfd021fb746804c95737f5e1da
Matthew Weil <mgweil29@thomasmore.edu>
2023-05-02 17:26:27 -0400

    added messaging for publishers/writers/editors

--- NewsAssignment/Pages/Inbox.cshtml.cs
+++ NewsAssignment/Pages/Inbox.cshtml.cs
@@ -1,15 +1,44 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NewsAssignment.Data;
+using NewsAssignment.Models;
 
 namespace NewsAssignment.Pages
 {
-   // [Authorize(Roles ="Admin,Writer,Publisher,Editor")]
+   [Authorize(Roles ="Admin,Writer,Publisher,Editor")]
     public class InboxModel : PageModel
     {
-        public void OnGet()
+        public UserManager<ApplicationUser> userManager;
+        ApplicationDbContext _context;
+
+        public InboxModel(UserManager<ApplicationUser> uManager, NewsAssignment.Data.ApplicationDbContext context)
         {
+            _context = context;
+            userManager = uManager;
+        }
+
+        public async Task OnGetAsync()
+
+        {
+            
+            var userrole = await userManager.GetRolesAsync(userManager.Users.Where(x => x.Email == User.Identity.Name).First());
+            SelectList roles = new SelectList(_context.Roles, "Id", "Name");
+            foreach (var role in roles)
+            {
+                string r = role.Text;
+                string u = userrole[0];
+                if(r == u)
+                {
+                    string x = role.Value;
+                 
+                    ViewData["UserRole"] = x;
+                    break;
+                }
+            }
+            
         }
     }
 }
--- NewsAssignment/Pages/SendMessage.cshtml.cs
+++ NewsAssignment/Pages/SendMessage.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,7 +12,7 @@ using System.Reflection;
 
 namespace NewsAssignment.Pages
 {
-    // [Authorize(Roles ="Admin,Writer,Publisher,Editor")]
+    [Authorize(Roles ="Admin,Writer,Publisher,Editor")]
     public class SendMessageModel : PageModel
     {
         private readonly IHubContext<NotificationHub> _notificationHubContext;
